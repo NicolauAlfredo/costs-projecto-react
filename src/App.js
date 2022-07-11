65409215ba0f69859fe710bf275d278c1c611b73
Nicolau Alfredo <nicolaualfredoalfredo@gmail.com>
2022-07-11 22:38:44 +0100

    Criando rotas no projecto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+
+import Container from './components/layout/Container'
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/contact">Contacto</Link>
+        <Link to="/newproject">Novo Projecto</Link>
+      </div>
+
+      <Routes> 
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/company' element={<Company />} />
+          <Route exact path='/contact' element={<Contact />} />
+          <Route exact path='/newproject' element={<NewProject />} /> 
+      </Routes>
+      <p>Footer</p>
+    </Router >
   );
 }
 
---
+++ src/components/pages/NewProject.js
@@ -0,0 +1,5 @@
+function NewProject() {
+    return <h1> Novo Projecto </h1>
+}
+
+export default NewProject
